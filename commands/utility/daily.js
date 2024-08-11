607c4480082770c3c615d46c4112e90690988151
Hieu Phung <142374885+hieudphung@users.noreply.github.com>
2024-08-11 01:58:00 -0400

    daily slash command

---
+++ commands/utility/daily.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const economy = require('../../economy/economy');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('daily')
+        .setDescription('Claim the daily coins reward.'),
+    
+    async execute(interaction) {
+        const userId = interaction.user.id;
+
+        // Give X coins
+        const reward = 5;
+        economy.addCoins(userId, reward);
+
+        // Bot Message
+        await interaction.reply(`${interaction.user.username}, you have claimed your daily reward of ${reward} coins!`);
+    },
+};
